a0f44352939a2f0328a53bccb4b339a110171438
laboratorioesan <laboratorioesan@esan.edu.pe>
2023-12-18 10:11:03 -0500

    Initial commit

---
+++ app/src/main/java/com/example/examenrezagadojarroyo/APIActivity.kt
@@ -0,0 +1,11 @@
+package com.example.examenrezagadojarroyo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class APIActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_apiactivity)
+    }
+}
---
+++ app/src/main/java/com/example/examenrezagadojarroyo/model/UsuarioModel.kt
@@ -0,0 +1,10 @@
+package com.example.examenrezagadojarroyo.model
+
+data class UsuarioModel(
+    val fecha: String = "",
+    val contraseña: String = "",
+    val correo: String = "",
+    val nombre: String = "",
+    val direccion: String ="",
+    val dni: String =""
+)
